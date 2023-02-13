9738076e85b6e0adfd836dd4e65abfa926d1196a
LeninQuintero <LeninQuinteroQuijano@gmail.com>
2023-02-13 12:06:23 -0300

    switch - ej integrador clase 7

---
+++ 10-EjerciciosIntegradores/ejercicio-integrador-clase7.js
@@ -0,0 +1,86 @@
+/*
+Lenin Quintero, Div 'D', grupo 2
+
+4.	Para el departamento de iluminación:
+Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
+A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
+B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
+C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+ ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
+ */
+
+ function CalcularPrecio() {
+    let cantidadLamp;
+    let marcaLamp;
+    let precioLamp;
+    let descuento;
+    let importeBruto;
+    let importeIIBB;
+    let valorDescuento;
+    let importeFinal;
+
+    cantidadLamp = document.getElementById('txtIdCantidad').value;
+    cantidadLamp = parseInt(cantidadLamp);
+
+    marcaLamp = document.getElementById('Marca').value;
+    descuento = 0;
+
+    precioLamp = 35;
+
+    switch (cantidadLamp) {
+
+        case 1:
+        case 2:
+            break;
+
+        case 3:
+            switch (marcaLamp) {
+                case 'ArgentinaLuz':
+                    descuento = 15;
+                    break;
+                case 'FelipeLamparas':
+                    descuento = 10;
+                    break;
+                default:
+                    descuento = 5;
+            }
+            break;
+
+        case 4:
+            switch (marcaLamp) {
+                case 'ArgentinaLuz':
+                case 'FelipeLamparas':
+                    descuento = 25;
+                    break;
+                default:
+                    descuento = 20;
+            }
+            break;
+
+        case 5:
+            switch (marcaLamp) {
+                case 'ArgentinaLuz':
+                    descuento = 40;
+                    break;
+                default:
+                    descuento = 30;
+            }
+            break;
+
+        default:
+            descuento = 50;
+    }
+
+    importeBruto = cantidadLamp * precioLamp;
+    valorDescuento = (importeBruto / 100) * descuento;
+    importeFinal = importeBruto - valorDescuento;
+    importeIIBB = (importeFinal / 100) * 10;
+    document.getElementById('txtIdprecioDescuento').value = importeFinal.toFixed(2);
+
+    if (importeFinal > 120) {
+        document.getElementById('txtIdprecioDescuento').value = (importeFinal + importeIIBB).toFixed(2);
+        alert('Usted pago ' + importeIIBB.toFixed(2) + ' de IIBB.');
+    }
+}
--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,30 @@
+/*
+Lenin Quintero, Div 'D', grupo 2
+
+al seleccionar un mes informar.
+si tiene 28 días.
+si tiene 30 días.
+si tiene 31 días.  
+*/
+
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	var mesDelAño = txtIdMes.value;
 
+	switch (mesDelAño) {
+		case 'Febrero':
+			alert('Este tiene 28 días.');
+			break;
 
+		case 'Abril':
+		case 'Junio':
+		case 'Septiembre':
+		case 'Noviembre':
+			alert('Este tiene 30 días.');
+			break;
 
+		default:
+			alert('Este mes tiene 31 días');
+	}
 }//FIN DE LA FUNCIÓN
