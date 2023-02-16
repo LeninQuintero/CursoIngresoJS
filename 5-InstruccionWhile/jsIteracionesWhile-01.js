6a2e43f3d571de963a6ac7466885026d71405c1c
LeninQuintero <LeninQuinteroQuijano@gmail.com>
2023-02-16 12:24:17 -0300

    ej while 1-5 & integrador 8

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -3,5 +3,17 @@ al presionar el botón mostrar 10 repeticiones
 con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
 {
-	alert('iteración while');
+	let numeroAscendente;
+	numeroAscendente = 0;
+
+	while (numeroAscendente < 10) {
+
+		document.write('El numero ascendente es: ' + (numeroAscendente + 1) + '<br>');
+		numeroAscendente++;
+
+		// Otras formas de aumentar***
+		// numeroAscendente += 1; 
+		// numeroAscendente = numeroAscendente + 1;
+	}
+
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -3,8 +3,12 @@ al presionar el botón mostrar 10 repeticiones
 con números DESCENDENTES, desde el 10 al 1.*/
 function mostrar()
 {
-	var contador;
-	contador=0; 
-	alert('iteración while');
+	let numeroDescendente;
+	numeroDescendente = 10;
+
+	while (numeroDescendente > 0) {
+		console.log('El numero descendente es: ' + numeroDescendente);
+		numeroDescendente--;
+	}
 
 }//FIN DE LA FUNCIÓN
